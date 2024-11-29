e75ee10530918ef1e88b737727a2fde87f1d0369
dotmeer <sidorovsy@yandex.ru>
2024-11-29 21:57:05 +0300

    refactor MetricsService

--- src/WbExtensions.Infrastructure/Metrics/MetricsService.cs
+++ src/WbExtensions.Infrastructure/Metrics/MetricsService.cs
@@ -1,35 +1,21 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Prometheus;
 using WbExtensions.Application.Interfaces.Metrics;
 
 namespace WbExtensions.Infrastructure.Metrics;
 
 internal sealed class MetricsService : IMetricsService
 {
-    private readonly ConcurrentDictionary<string, Counter> _counters = new();
-
-    private readonly ConcurrentDictionary<string, Gauge> _gauges = new();
-
     public void IncrementCounter(
         string name,
         IDictionary<string, string>? labels = null,
         string? description = null)
     {
-        if (!_counters.TryGetValue(name, out var counter))
-        {
-            counter = Prometheus.Metrics.CreateCounter(
+        Prometheus.Metrics.CreateCounter(
                 name,
                 description ?? name,
-                labels?.Keys.ToArray() ?? Array.Empty<string>());
-
-            _counters[name] = counter;
-        }
-
-        counter
-            .WithLabels(labels?.Values.ToArray() ?? Array.Empty<string>())
+                labels?.Keys.ToArray() ?? [])
+            .WithLabels(labels?.Values.ToArray() ?? [])
             .Inc();
     }
 
@@ -39,18 +25,11 @@ internal sealed class MetricsService : IMetricsService
         IDictionary<string, string>? labels = null,
         string? description = null)
     {
-        if (!_gauges.TryGetValue(name, out var gauge))
-        {
-            gauge = Prometheus.Metrics.CreateGauge(
+        Prometheus.Metrics.CreateGauge(
                 name,
                 description ?? name,
-                labels?.Keys.ToArray() ?? Array.Empty<string>());
-
-            _gauges[name] = gauge;
-        }
-
-        gauge
-            .WithLabels(labels?.Values.ToArray() ?? Array.Empty<string>())
+                labels?.Keys.ToArray() ?? [])
+            .WithLabels(labels?.Values.ToArray() ?? [])
             .Set(value);
     }
 }
